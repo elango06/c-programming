00d63c77bdd7b609c73c2f0d4fcb838ce12c4f8d
elango06 <37832080+elango06@users.noreply.github.com>
2018-03-27 15:01:27 +0530

    Create oddoreven.c

---
+++ beginner/oddoreven.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main(void) {
+		int a;
+	printf(" Enter the number:");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("number is even");
+	}
+	else
+	{
+		printf("number is odd");
+	}
+	return 0;
+}
